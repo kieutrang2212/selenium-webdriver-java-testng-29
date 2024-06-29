ce231e68d3e8ecc21d488e102f095f9398c4b41e
Nguyen Thi Kieu Trang <nguyenthikieutrangtt12212@gmail.com>
2024-06-29 13:40:44 +0700

    test

---
+++ selenium/webdriver/Topic_09_Default_Dropdown.java
@@ -0,0 +1,107 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Topic_09_Default_Dropdown {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+    String firstName="Trang", lastName="Nguyen", emailAddress=getEmailAddress();
+    String companyName="Selenium", password="123456";
+    String day="11", month="February",year="1923";
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        }
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/");
+
+    }
+
+    @Test
+    public void TC_01_Register() {
+        driver.findElement(By.cssSelector("a.ico-register")).click();
+
+        driver.findElement(By.id("FirstName")).sendKeys(firstName);
+        driver.findElement(By.id("LastName")).sendKeys(lastName);
+
+        Select day = new Select(driver.findElement(By.name("DateOfBirthDay")));
+        day.selectByVisibleText(this.day);
+        Assert. assertEquals(day.getOptions().size(),32);
+
+        Select month = new Select(driver.findElement(By.name("DateOfBirthMonth")));
+        month.selectByVisibleText(this.month);
+        Assert. assertEquals(month.getOptions().size(),13);
+
+        new Select(driver.findElement(By.name("DateOfBirthYear"))).selectByVisibleText(this.year);
+
+        driver.findElement(By.id("Email")).sendKeys(emailAddress);
+        driver.findElement(By.id("Company")).sendKeys(companyName);
+        driver.findElement(By.id("Password")).sendKeys(password);
+        driver.findElement(By.id("ConfirmPassword")).sendKeys(password);
+
+        driver.findElement(By.id("register-button")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.result")).getText(),"Your registration completed");
+
+    }
+
+    @Test
+    public void TC_02_Login() {
+        driver.get("https://demo.nopcommerce.com/");
+
+        sleepInSeconds(5);
+        driver.findElement(By.cssSelector("a.ico-login")).click();
+        sleepInSeconds(2);
+        driver.findElement(By.id("Email")).sendKeys(emailAddress);
+        driver.findElement(By.id("Password")).sendKeys(password);
+        driver.findElement(By.cssSelector("button.login-button")).click();
+        sleepInSeconds(3);
+        driver.findElement(By.cssSelector("a.ico-account")).click();
+        sleepInSeconds(3);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("input#FirstName")).getAttribute("value"),firstName);
+        Assert.assertEquals(driver.findElement(By.cssSelector("input#LastName")).getAttribute("value"),lastName);
+        Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthDay"))).getFirstSelectedOption().getText(),this.day);
+        Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthMonth"))).getFirstSelectedOption().getText(),this.month);
+        Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthYear"))).getFirstSelectedOption().getText(),this.year);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("input#Email")).getAttribute("value"),emailAddress);
+
+    }
+    public void sleepInSeconds(long timeInSecond){
+        try {
+            Thread.sleep(timeInSecond*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getEmailAddress(){
+        Random rand= new Random();
+        String emailAddress="trang"+rand.nextInt(99999)+"@gmail.com";
+        return emailAddress;
+    }
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+}
